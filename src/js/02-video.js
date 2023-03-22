02a8aba963521156c9e066aa1e4b0fe01bca819c
Borovyk-Mykola <114910601+Borovyk-Mykola@users.noreply.github.com>
2023-03-22 23:39:14 +0200

    finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,17 +3,15 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function() {
+player.on('play', () => {
     console.log('played the video!');
 });
 
 const CURRENT_TIME_KEY = 'videoplayer-current-time';
-
-const getCurrentTime = function (currentTime) {
+  
+player.on('timeupdate', (currentTime) => {
     const seconds = currentTime.seconds;
     localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
-};
-  
-player.on('timeupdate', getCurrentTime);
+});
   
 player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
